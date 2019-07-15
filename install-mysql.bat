a5d2570aa17b77f6e674c17176b7f69103f4ace7
CodeDifferently <leonhunter@Leons-MacBook-Pro-2.local>
2019-07-15 14:47:43 -0400

    first commit

---
+++ install-mysql.bat
@@ -0,0 +1,11 @@
+:: download 7zip
+chrome https://www.7-zip.org/a/7z1900-x64.exe
+
+:: install 7zip
+start 7z1900-x64.exe
+
+:: download MySQL 5.7.26
+chrome https://dev.mysql.com/get/Downloads/MySQL-5.7/mysql-5.7.26-winx64.zip
+
+:: unzip download
+7z.exe x mysql-5.7.26-winx64.zip
